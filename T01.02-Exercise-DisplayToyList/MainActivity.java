d92b161a03a207f45c3503894012211d1e6f378f
poplopo <aethereal@gmail.com>
2018-01-12 14:44:37 -0800

    Update MainActivity.java
    Completed all TODO's

--- T01.02-Exercise-DisplayToyList/MainActivity.java
+++ T01.02-Exercise-DisplayToyList/MainActivity.java
@@ -28,11 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (3) Use findViewById to get a reference to the TextView from the layout
-        mToysListTextView = findViewById();
+        // COMPLETE (3) Use findViewById to get a reference to the TextView from the layout
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
 
-        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+        // COMPLETE (4) Use the static ToyBox.getToyNames method and store the names in a String array
+        String[] toyNames = ToyBox.getToyNames();
 
-        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        // COMPLETE (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        for (int i = 0; i < toyNames.length; i++)
+        {
+            mToysListTextView.append("\n" + toyNames[i]);
+        }
     }
 }
